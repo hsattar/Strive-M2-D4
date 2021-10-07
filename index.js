8c9e8b1bc151b234a0a9714b44f310cd37303d0d
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-07 14:35:08 +0100

    Added Ability To Create & Display Team List

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 let playerNames = [ ]
+let noOfTeams = 0
 
 let playersNameInput = document.getElementById('players-name-input')
 let addPlayerName = document.getElementById('add-player-name')
 let noOfTeamsInput = document.getElementById('no-of-teams-input')
 let createNoOfTeams = document.getElementById('create-no-of-teams')
 
-const triggerClickWithEnter = (input, btn) => {
-    input.onkeyup = e => {
-        if (e.keyCode === 13) {
-            e.preventDefault()
-            btn.click()
-        }
-    }
-}
 
-triggerClickWithEnter(playersNameInput, addPlayerName)
-
+// ADDING & DISPLAYING THE PLAYERS IN WAITING AREA
 
 const displayPlayerInWaitingArea = () => {
     let waitingListArea = document.getElementById('waiting-list')
@@ -35,5 +27,36 @@ const addPlayerToArray = () => {
 }
 
 
+// CREATING THE TEAMS
+
+const createTeams =() => {
+    noOfTeams = noOfTeamsInput.value
+    noOfTeamsInput.value = ''
+    let displayTeamsArea = document.getElementById('display-teams-area')
+    for (let i = 1; i <= noOfTeams; i++) {
+        let displayTeamDiv = document.createElement('div')
+        displayTeamDiv.className = 'col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2'
+        displayTeamDiv.innerHTML = `
+            <ul class="list-group">
+                <li class="list-group-item active">Team ${i}</li>
+            </ul>`
+        displayTeamsArea.appendChild(displayTeamDiv)
+    }
+}
+
+// BUTTON CLICK EVENT LISTENERS
+
+const triggerClickWithEnter = (input, btn) => {
+    input.onkeyup = e => {
+        if (e.keyCode === 13) {
+            e.preventDefault()
+            btn.click()
+        }
+    }
+}
+
+triggerClickWithEnter(playersNameInput, addPlayerName)
+triggerClickWithEnter(playersNameInput, addPlayerName)
 
 addPlayerName.addEventListener('click', addPlayerToArray)
+createNoOfTeams.addEventListener('click', createTeams)
