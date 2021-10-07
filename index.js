98a91419b588c530a9330f3eac8853753e14049f
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-07 15:03:47 +0100

    Added Ability To Assign Players To Teams One At a Time

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 let playerNames = [ ]
 let noOfTeams = 0
+let assignPlayerClicks = 0
 
 let playersNameInput = document.getElementById('players-name-input')
-let addPlayerName = document.getElementById('add-player-name')
 let noOfTeamsInput = document.getElementById('no-of-teams-input')
-let createNoOfTeams = document.getElementById('create-no-of-teams')
 
 
 // ADDING & DISPLAYING THE PLAYERS IN WAITING AREA
 
-const displayPlayerInWaitingArea = () => {
+const displayPlayersInWaitingArea = () => {
     let waitingListArea = document.getElementById('waiting-list')
     waitingListArea.innerHTML = ''
     for (player of playerNames) {
@@ -23,7 +22,7 @@ const displayPlayerInWaitingArea = () => {
 const addPlayerToArray = () => {
     playerNames.push(playersNameInput.value)
     playersNameInput.value = ''
-    displayPlayerInWaitingArea()
+    displayPlayersInWaitingArea()
 }
 
 
@@ -37,14 +36,40 @@ const createTeams =() => {
         let displayTeamDiv = document.createElement('div')
         displayTeamDiv.className = 'col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2'
         displayTeamDiv.innerHTML = `
-            <ul class="list-group">
+            <ul id="team${i}" class="list-group">
                 <li class="list-group-item active">Team ${i}</li>
             </ul>`
         displayTeamsArea.appendChild(displayTeamDiv)
     }
 }
 
+
+// ASSIGN PLAYERS TO TEAMS
+
+const assignPlayerToTeam = () => {
+    console.log(playerNames)
+    assignPlayerClicks++
+    if (assignPlayerClicks > noOfTeams) {
+        assignPlayerClicks = 1
+    }
+    let randomPlayer = Math.floor(Math.random() * playerNames.length)
+    let newMember = document.createElement('li')
+    newMember.innerText = playerNames[randomPlayer]
+    playerNames.splice(randomPlayer, 1)
+    let chosenTeam = document.getElementById(`team${assignPlayerClicks}`)
+    chosenTeam.appendChild(newMember)
+    console.log(playerNames)
+    displayPlayersInWaitingArea()
+}
+
+
+
 // BUTTON CLICK EVENT LISTENERS
+
+let addPlayerName = document.getElementById('add-player-name')
+let createNoOfTeams = document.getElementById('create-no-of-teams')
+let assignPLayer = document.getElementById('assign-player')
+let assignAll = document.getElementById('assign-all')
 
 const triggerClickWithEnter = (input, btn) => {
     input.onkeyup = e => {
@@ -56,7 +81,8 @@ const triggerClickWithEnter = (input, btn) => {
 }
 
 triggerClickWithEnter(playersNameInput, addPlayerName)
-triggerClickWithEnter(playersNameInput, addPlayerName)
+triggerClickWithEnter(noOfTeamsInput, createNoOfTeams)
 
 addPlayerName.addEventListener('click', addPlayerToArray)
 createNoOfTeams.addEventListener('click', createTeams)
+assignPLayer.addEventListener('click', assignPlayerToTeam)
