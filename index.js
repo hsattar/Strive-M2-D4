7e2752498dd279c582c6a470bfba7591e545475b
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-07 14:08:55 +0100

    Will now display user in waiting area when added

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let playersNameInput = document.getElementById('players-name-input')
 let addPlayerName = document.getElementById('add-player-name')
 let noOfTeamsInput = document.getElementById('no-of-teams-input')
 let createNoOfTeams = document.getElementById('create-no-of-teams')
-let waitingListArea = document.getElementById('waiting-list')
 
 const triggerClickWithEnter = (input, btn) => {
     input.onkeyup = e => {
@@ -17,11 +16,24 @@ const triggerClickWithEnter = (input, btn) => {
 
 triggerClickWithEnter(playersNameInput, addPlayerName)
 
-const addPlayer = () => {
-    playerNames.push(playersNameInput.value)
-    console.log(playerNames)
-    playersNameInput.value = ''
+
+const displayPlayerInWaitingArea = () => {
+    let waitingListArea = document.getElementById('waiting-list')
+    waitingListArea.innerHTML = '<h2>Waiting List</h2>'
+    for (player of playerNames) {
+        let para = document.createElement('p')
+        para.innerHTML += player
+        waitingListArea.appendChild(para)
+    }
 }
 
 
-addPlayerName.addEventListener('click', addPlayer)
+const addPlayerToArray = () => {
+    playerNames.push(playersNameInput.value)
+    playersNameInput.value = ''
+    displayPlayerInWaitingArea()
+}
+
+
+
+addPlayerName.addEventListener('click', addPlayerToArray)
